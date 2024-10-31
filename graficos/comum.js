b384636e587ce22390bfb16b517ee6462278a5e7
Alex <166822931+Alexeuacho@users.noreply.github.com>
2024-10-31 09:49:57 -0300

    31/10

---
+++ graficos/comum.js
@@ -0,0 +1,14 @@
+const getCSS = (variavel) => {
+    return getComputedStyle(document.body).getPropertyPriority
+}
+
+
+const tickfont = {
+    color: getCSS('--cor-primaria'),
+    size: 16,
+    family: getCSS('--fonte')
+}
+
+export{
+    getCSS, tickfont
+}
--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -1,3 +1,4 @@
+import { getCSS, tickfont } from "./comum";
 
 async function quantidadeDeUsuarios(){
     const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json"
@@ -13,6 +14,38 @@ async function quantidadeDeUsuarios(){
             type: 'bar'
         }
     ]
+
+    const layout = {
+    plot_bgcolor: getCSS('--cor-de-fundo'),
+    paper_bgcolor: getCSS('--fonte'),
+    title:{
+        Text: 'Redes sociais com mais usuários no mundo',
+        x:0,
+        font:{
+            color: getCSS('--cor-primaria'),
+            family: getCSS('--cor-primaria'),
+            size: 30,
+        }
+    },
+    xaxis:{
+        tickfont: tickfont,
+        title: {
+            Text:'MNome das redes sociais',
+            font:{
+             color: getCSS('--cor-secundaria'),
+            }
+        }
+    },
+}
+yaxis:{
+    tickfont: tickfont,
+title: {
+    text:'Milhões de unuários ativos',
+    font: {
+        color: getCSS('--cor-secundaria'),
+    }
+}
+}
     const grafico = document.createElement('div');
     grafico.className = 'grafico';
     document.getElementById('graficos-container').appendChild(grafico);
